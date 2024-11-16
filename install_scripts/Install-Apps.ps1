a1d51208889300781b322630ae517dd1dacbf56a
Crusader13 <39165985+Crusader13@users.noreply.github.com>
2024-11-16 11:54:16 +0100

    Update Install-Apps.ps1
    add more applications

--- install_scripts/Install-Apps.ps1
+++ install_scripts/Install-Apps.ps1
@@ -3,7 +3,8 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     return
 }
 
-$wingetInstallAppIds = "JetBrains.RustRover",
+$wingetInstallAppIds =
+"JetBrains.RustRover",
 "Microsoft.VisualStudio.2022.Community",
 "RiotGames.Valorant.EU",
 "RiotGames.LeagueOfLegends.EUW",
@@ -11,7 +12,32 @@ $wingetInstallAppIds = "JetBrains.RustRover",
 "Proton.ProtonPass",
 "Microsoft.OpenJDK.21",
 "LLVM.LLVM",
-"CoreyButler.NVMforWindows"
+"CoreyButler.NVMforWindows",
+"Microsoft.PowerShell",
+"Microsoft.VCRedist.2015+.x64",
+"VSCodium.VSCodium",
+"rcmaehl.MSEdgeRedirect",
+"Notepad++.Notepad++",
+"Neovim.Neovim",
+"VideoLAN.VLC",
+"Git.Git",
+"Hibbiki.Chromium",
+"Valve.Steam",
+"GitHub.GitHubDesktop",
+"JanDeDobbeleer.OhMyPosh",
+"Gyan.FFmpeg",
+"Guru3D.Afterburner",
+"7zip.7zip",
+"ajeetdsouza.zoxide",
+"ShareX.ShareX",
+"Nextcloud.NextcloudDesktop",
+"yt-dlp.yt-dlp",
+"Microsoft.WindowsTerminal",
+"OBSProject.OBSStudio",
+"gerardog.gsudo",
+"Discord.Discord",
+"Microsoft.Teams"
+
 
 $downloadUrls = "https://win.rustup.rs/x86_64"
 
